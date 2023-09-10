d536fe4f36e1006e9d8e09be18efdc47580e5693
João Vitor Dias <joaovdias55@gmail.com>
2023-09-09 22:23:43 -0300

    .

--- pokedex-project/src/Components/Header.jsx
+++ pokedex-project/src/Components/Header.jsx
@@ -1,10 +1,26 @@
 import { useState } from 'react'
+import { useEffect } from 'react'
 import React from 'react'
+import axios from 'axios'
 import Pokecard from './Pokecard'
+import { Container, Grid } from '@mui/material'
+
 
 
 function Header () {
+    const [pokemons, setPokemons] = useState([])
+    useEffect(() => {
+         getPokemons();
+    }, [])
+        const getPokemons = () => { 
+        var endpoints = []
+        for (var i = 1; i++; i<50 )
+    axios.get("https://pokeapi.co/api/v2/pokemon?limit=50")
+    .then((res) => setPokemons(res.data.results))
+    .catch((err) => console.log(err));
+    };
     return (
+    
         <>
             <h1 id='logo'>
                 Pokédex
@@ -12,7 +28,28 @@ function Header () {
             <form action="form" className='form'>
                 <input type="search" className='input_search' placeholder='Name or Number' required />
             </form>
-            <Pokecard />
+            <Container maxWidth='xg'>
+                <Grid container>
+                    {pokemons.map((pokemon, key) => (
+                        <Grid item xs={3} key={key}>
+                        <Pokecard name={pokemon.name} />
+                    </Grid>
+                    ))}
+                    <Grid item xs={3}>
+                        <Pokecard />
+                    </Grid>
+                    <Grid item xs={3}>
+                        <Pokecard />
+                    </Grid>
+                    <Grid item xs={3}>
+                        <Pokecard />
+                    </Grid>
+                    <Grid item xs={3}>
+                        <Pokecard />
+                    </Grid>
+                </Grid>
+
+            </Container>
             <div className='buttons'>
                 <button className="button btn-prev">&lt; Prev</button>
                 <button className="button btn-next">Next &gt;</button>
--- pokedex-project/src/Components/Pokecard.jsx
+++ pokedex-project/src/Components/Pokecard.jsx
@@ -1,16 +1,32 @@
-import { useState } from 'react'
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
-function Pokecard () {
-    return (
-        <>
-            <img src="pokemon" alt="pokemon__image" />
-
-            <h1 className='pokemon_data'>
-                <span className='pokemon_number'></span> -
-                <span className='pokemon-name'></span>
-            </h1>
-        </>
-    )
+export default function Pokecard({ name }) {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        sx={{ height: 140 }}
+        image="/static/images/cards/contemplative-reptile.jpg"
+        title="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {name}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {/* Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica */}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        {/* <Button size="small">Share</Button>
+        <Button size="small">Learn More</Button> */}
+      </CardActions>
+    </Card>
+  );
 }
-
-export default Pokecard
